9a7ca62ea2a794a480cf8f18bdcfac28910acb86
debobrad579 <79882180+debobrad579@users.noreply.github.com>
2023-02-08 22:27:13 -0500

    Update date-formatter.py

--- date-formatter.py
+++ date-formatter.py
@@ -1,3 +1,4 @@
+# 30-11-22
 months = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "Augest", 9: "September", 10: "October", 11: "November", 12: "December"}
 total_days = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
 day_suffixes = {"0": "th", "1": "st", "2": "nd", "3": "rd", "4": "th", "5": "th", "6": "th", "7": "th", "8": "th", "9": "th"}
