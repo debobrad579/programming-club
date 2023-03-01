7ce24232374e076545b7eadf93a91892f0eb1cad
debobrad579 <79882180+debobrad579@users.noreply.github.com>
2023-03-01 16:52:29 -0500

    Update tictactoe.py

--- tictactoe-v1/tictactoe.py
+++ tictactoe-v1/tictactoe.py
@@ -1,44 +1,36 @@
 from copy import deepcopy
 
-X, O, EMPTY = "X", "O", None
-
-
 def initial_state():
     return [
-        [EMPTY, EMPTY, EMPTY],
-        [EMPTY, EMPTY, EMPTY],
-        [EMPTY, EMPTY, EMPTY]
+        [None, None, None],
+        [None, None, None],
+        [None, None, None]
     ]
 
-
-def result(board, action):
-    if board[action[0]][action[1]] != EMPTY:
-        raise Exception("'{action}' is not a valid action.")
-
+def result(board, move):
     result = deepcopy(board)
-    result[action[0]][action[1]] = player(board)
+    result[move[0]][move[1]] = player(board)
     return result
 
+def player(board):
+    return "X" if (
+        board[0].count("X") + board[1].count("X") + board[2].count("X")
+        == board[0].count("O") + board[1].count("O") + board[2].count("O")
+    ) else "O"
 
 def winner(board):
-    for i in board + [[board[j][i] for j in range(len(board))] for i in range(len(board[0]))]:
-        if i.count(X) == 3: return X
-        if i.count(O) == 3: return O
-    
-    if board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]: return board[1][1]
+    for i in board + [[board[j][i] for j in range(len(board))] for i in range(len(board))]:
+        if i.count("X") == 3:
+            return "X"
+        if i.count("O") == 3:
+            return "O"
 
-    return None
-
+        if board[0][0] == board[1][1] == board[2][2] or board[2][0] == board[1][1] == board[0][2]:
+            return board[1][1]
 
 def terminal(board):
-    return (
-        board[0].count(EMPTY) + board[1].count(EMPTY) + board[2].count(EMPTY) == 0 
-        or winner(board) != None
-    )
-
-
-def player(board):
-    return X if (
-        board[0].count(X) + board[1].count(X) + board[2].count(X) 
-        == board[0].count(O) + board[1].count(O) + board[2].count(O)
-    ) else O
+    if winner(board) is not None or board[0].count(None) + board[1].count(None) + board[2].count(None) == 0:
+        return True
+    else:
+        return False
+    
